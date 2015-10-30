66b2ad02a6fb7649fe5d4a13e06de01c04387944
nezharen <nezharen@163.com>
2015-10-30 14:23:50 +0800

    DataVision

---
+++ DataVision/DataType.java
@@ -0,0 +1,33 @@
+
+public class DataType {
+
+	public int temp;
+	public int humid;
+	public int light;
+	public long seqid;
+	public long time;
+
+	public DataType(int temp, int humid, int light, long seqid, long time) {
+		this.temp = temp;
+		this.humid = humid;
+		this.light = light;
+		this.seqid = seqid;
+		this.time = time;
+	}
+	
+	public double getPhysicalTemp() {
+		int SOT = this.temp & 16383;
+		return (-39.6 + 0.01 * SOT);
+	}
+	
+	public double getPhysicalHumid() {
+		int SORH = this.humid & 4095;
+		double temp = this.getPhysicalTemp();
+		double linear = -4 + 0.0405 * SORH - 2.8e-6 * SORH * SORH;
+		return ((temp - 25) * (0.01 + 0.00008 * SORH) + linear);
+	}
+	
+	public double getPhysicalLight() {
+		return (0.085 * this.light);
+	}
+}
--- README.txt
+++ README.txt
@@ -9,6 +9,7 @@ Computer Networking Project 2
 文件结构：
 Sense/ -- 节点源代码
 BaseStation/ -- 基站源代码
+DataVision/ -- 数据可视化源代码
 
 BaseStation
 使用make telosb install,0刷入0号节点
@@ -17,4 +18,6 @@ Sense
 使用make telosb install,1刷入1号节点
 使用make telosb install,2刷入2号节点
 
+DataVision
+make后执行./run。需要提前启动java net.tinyos.sf.SerialForwarder。
 
